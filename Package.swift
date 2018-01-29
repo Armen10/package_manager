4e6bc44e4d6c7a644a729db3c9373a36c3b2e59a
ahakobya <armen.hakobyan@oracle.com>
2018-01-29 14:29:30 +0400

    Change PackageSwiftDescription

--- Package.swift
+++ Package.swift
@@ -4,17 +4,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "package_manager",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package( name: "package_manager", targets: ["package_manager"]),
-
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "package_manager",
-            dependencies: []),
-    ]
+    name: "package_manager"
 )
