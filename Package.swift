838908e839730531b49950924f0ece8f0c920f9b
ahakobya <armen.hakobyan@oracle.com>
2018-01-29 14:41:22 +0400

    Initial version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "package_manager",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .library( name: "package_manager", targets: ["package_manager"]),
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
